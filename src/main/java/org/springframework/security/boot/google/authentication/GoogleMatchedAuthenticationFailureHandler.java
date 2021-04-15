ac9a3d7e4a3b9d060acb365d6e1161bab9296b55
wandl <hiwepy@163.com>
2021-04-15 17:01:20 +0800

    国际化信息

--- src/main/java/org/springframework/security/boot/google/authentication/GoogleMatchedAuthenticationFailureHandler.java
+++ src/main/java/org/springframework/security/boot/google/authentication/GoogleMatchedAuthenticationFailureHandler.java
@@ -1,24 +1,55 @@
 package org.springframework.security.boot.google.authentication;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.context.support.MessageSourceAccessor;
-import org.springframework.security.boot.biz.SpringSecurityBizMessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationFailureHandler;
+import org.springframework.security.boot.biz.exception.AuthResponse;
+import org.springframework.security.boot.biz.exception.AuthResponseCode;
+import org.springframework.security.boot.google.SpringSecurityGoogleMessageSource;
 import org.springframework.security.boot.google.exception.GoogleAcceccTokenNotFoundException;
 import org.springframework.security.boot.google.exception.GoogleIdTokenVerifierException;
 import org.springframework.security.boot.utils.SubjectUtils;
 import org.springframework.security.core.AuthenticationException;
+
+import com.alibaba.fastjson.JSONObject;
 
 /**
  * Google认证请求失败后的处理实现
  */
 public class GoogleMatchedAuthenticationFailureHandler implements MatchedAuthenticationFailureHandler {
 
-	protected MessageSourceAccessor messages = SpringSecurityBizMessageSource.getAccessor();
+	protected MessageSourceAccessor messages = SpringSecurityGoogleMessageSource.getAccessor();
 	
 	@Override
 	public boolean supports(AuthenticationException e) {
 		return SubjectUtils.isAssignableFrom(e.getClass(), GoogleAcceccTokenNotFoundException.class, 
 				GoogleIdTokenVerifierException.class);
+	}
+
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException e) throws IOException, ServletException {
+
+		response.setStatus(HttpStatus.OK.value());
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+		
+		if (e instanceof GoogleAcceccTokenNotFoundException) {
+			JSONObject.writeJSONString(response.getWriter(), AuthResponse.of(AuthResponseCode.SC_AUTHZ_CODE_REQUIRED.getCode(), 
+					messages.getMessage(AuthResponseCode.SC_AUTHZ_CODE_REQUIRED.getMsgKey(), e.getMessage())));
+		} else if (e instanceof GoogleIdTokenVerifierException) {
+			JSONObject.writeJSONString(response.getWriter(), AuthResponse.of(AuthResponseCode.SC_AUTHZ_CODE_INVALID.getCode(), 
+					messages.getMessage(AuthResponseCode.SC_AUTHZ_CODE_INVALID.getMsgKey(), e.getMessage())));
+		}
+		
 	}
 	
 }
