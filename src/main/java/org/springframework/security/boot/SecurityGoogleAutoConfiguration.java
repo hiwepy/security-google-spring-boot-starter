eacd551de782cc86f15935cad13ced856ee8ce0d
wandl <hiwepy@163.com>
2021-06-01 20:51:18 +0800

    x

--- src/main/java/org/springframework/security/boot/SecurityGoogleAutoConfiguration.java
+++ src/main/java/org/springframework/security/boot/SecurityGoogleAutoConfiguration.java
@@ -1,22 +1,35 @@
 package org.springframework.security.boot;
 
 import java.security.GeneralSecurityException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.PropertyMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.boot.biz.authentication.AuthenticationListener;
+import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationEntryPoint;
+import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationFailureHandler;
+import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationSuccessHandler;
+import org.springframework.security.boot.biz.property.SecuritySessionMgtProperties;
 import org.springframework.security.boot.biz.userdetails.JwtPayloadRepository;
 import org.springframework.security.boot.biz.userdetails.UserDetailsServiceAdapter;
+import org.springframework.security.boot.google.authentication.GoogleAuthenticationProcessingFilter;
 import org.springframework.security.boot.google.authentication.GoogleAuthenticationProvider;
 import org.springframework.security.boot.google.authentication.GoogleMatchedAuthenticationEntryPoint;
 import org.springframework.security.boot.google.authentication.GoogleMatchedAuthenticationFailureHandler;
 import org.springframework.security.boot.google.authentication.GoogleMatchedAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.util.StringUtils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.client.googleapis.auth.oauth2.GooglePublicKeysManager;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
@@ -85,5 +98,59 @@ public class SecurityGoogleAutoConfiguration {
 	public GoogleAuthenticationProvider googleAuthenticationProvider(UserDetailsServiceAdapter userDetailsService) {
 		return new GoogleAuthenticationProvider(userDetailsService);
 	}
+	
+	@Bean
+	public GoogleAuthenticationProcessingFilter authenticationProcessingFilter(
+			
+			SecurityGoogleAuthcProperties authcProperties,
+			SecuritySessionMgtProperties sessionMgtProperties,
+			
+			ObjectProvider<GooglePublicKeysManager> publicKeysManagerProvider,
+			ObjectProvider<Clock> clockProvider,
+			ObjectProvider<AuthenticationProvider> authenticationProvider,
+			ObjectProvider<AuthenticationManager> authenticationManagerProvider,
+			ObjectProvider<AuthenticationListener> authenticationListenerProvider,
+			ObjectProvider<MatchedAuthenticationEntryPoint> authenticationEntryPointProvider,
+			ObjectProvider<MatchedAuthenticationSuccessHandler> authenticationSuccessHandlerProvider,
+			ObjectProvider<MatchedAuthenticationFailureHandler> authenticationFailureHandlerProvider,
+			ObjectProvider<ObjectMapper> objectMapperProvider,
+			ObjectProvider<RememberMeServices> rememberMeServicesProvider) throws Exception {
+    	
+		GooglePublicKeysManager publicKeysManager = publicKeysManagerProvider.getIfAvailable();
+		Clock clock = clockProvider.getIfAvailable(() -> { return Clock.SYSTEM; });
+		List<AuthenticationListener> authenticationListeners = authenticationListenerProvider.stream().collect(Collectors.toList());
+		MatchedAuthenticationEntryPoint authenticationEntryPoint = super.authenticationEntryPoint(authenticationEntryPointProvider.stream().collect(Collectors.toList()));
+			this.authenticationSuccessHandler = super.authenticationSuccessHandler(authenticationListeners, authenticationSuccessHandlerProvider.stream().collect(Collectors.toList()));
+			this.authenticationFailureHandler = super.authenticationFailureHandler(authenticationListeners, authenticationFailureHandlerProvider.stream().collect(Collectors.toList()));
+			this.objectMapper = objectMapperProvider.getIfAvailable();
+			this.requestCache = super.requestCache();
+			this.rememberMeServices = rememberMeServicesProvider.getIfAvailable();
+			this.sessionAuthenticationStrategy = super.sessionAuthenticationStrategy();
+			
+		GoogleAuthenticationProcessingFilter authenticationFilter = new GoogleAuthenticationProcessingFilter(objectMapper);
+		
+		/**
+		 * 批量设置参数
+		 */
+		PropertyMapper map = PropertyMapper.get().alwaysApplyingWhenNonNull();
+		
+		map.from(sessionMgtProperties.isAllowSessionCreation()).to(authenticationFilter::setAllowSessionCreation);
+		
+		map.from(authenticationManager).to(authenticationFilter::setAuthenticationManager);
+		map.from(authenticationSuccessHandler).to(authenticationFilter::setAuthenticationSuccessHandler);
+		map.from(authenticationFailureHandler).to(authenticationFilter::setAuthenticationFailureHandler);
+		
+		map.from(authcProperties.getClientIds()).to(authenticationFilter::setClientIds);
+		map.from(this.publicKeysManager).to(authenticationFilter::setPublicKeysManager);
+		map.from(this.clock).to(authenticationFilter::setClock);
+		map.from(authcProperties.getAcceptableTimeSkewSeconds()).to(authenticationFilter::setAcceptableTimeSkewSeconds);
+		
+		map.from(authcProperties.getPathPattern()).to(authenticationFilter::setFilterProcessesUrl);
+		map.from(rememberMeServices).to(authenticationFilter::setRememberMeServices);
+		map.from(sessionAuthenticationStrategy).to(authenticationFilter::setSessionAuthenticationStrategy);
+		map.from(authcProperties.isContinueChainBeforeSuccessfulAuthentication()).to(authenticationFilter::setContinueChainBeforeSuccessfulAuthentication);
+		
+        return authenticationFilter;
+    }
 
 }
