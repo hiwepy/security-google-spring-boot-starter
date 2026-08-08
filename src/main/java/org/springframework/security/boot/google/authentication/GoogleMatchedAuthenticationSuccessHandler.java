0cbd5a114e4e5ad8bc70da9eb1471b287c26b5db
wandl-6A72h <hiwepy@gmail.com>
2026-08-09 00:01:42 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 4.0.x branch

--- src/main/java/org/springframework/security/boot/google/authentication/GoogleMatchedAuthenticationSuccessHandler.java
+++ src/main/java/org/springframework/security/boot/google/authentication/GoogleMatchedAuthenticationSuccessHandler.java
@@ -23,7 +23,7 @@ import java.nio.charset.StandardCharsets;
 
 /**
  * Google 认证 (authentication)成功回调器：讲认证信息写回前端
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class GoogleMatchedAuthenticationSuccessHandler implements MatchedAuthenticationSuccessHandler {
 
