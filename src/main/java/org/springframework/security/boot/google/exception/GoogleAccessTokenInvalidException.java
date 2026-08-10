250ad96b910284cb29886e9fc4205d85c3c6fa1d
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:57:03 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/springframework/security/boot/google/exception/GoogleAccessTokenInvalidException.java
+++ src/main/java/org/springframework/security/boot/google/exception/GoogleAccessTokenInvalidException.java
@@ -19,7 +19,7 @@ import org.springframework.security.boot.biz.exception.AuthenticationTokenInvali
 
 /**
  * Google AccessToken Invalid Exception
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @SuppressWarnings("serial")
 public class GoogleAccessTokenInvalidException extends AuthenticationTokenInvalidException {
